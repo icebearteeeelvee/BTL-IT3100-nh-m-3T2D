08edb3d8554874c47b46e0d186ec0d10328ed0ed
IvanDuDu <Dvu3999@gmail.com>
2024-04-19 09:07:12 +0700

    Push Pedestrian file

---
+++ src/pedastrian/pedestrian.cpp
@@ -0,0 +1,263 @@
+#include "pedestrian.h"
+#include "lib/nlohmann/json.hpp"
+
+#include <iostream>
+#include <vector>
+#include <cmath>
+#include <fstream>
+#include <random>
+
+
+using namespace std;
+using json = nlohmann::json;
+
+double randomDouble(double min, double max) {
+	random_device rd;
+	mt19937 gen(rd());
+	uniform_real_distribution<double> dis(min, max);
+	return dis(gen);
+}
+
+int randomInt(int min, int max) {
+	random_device rd;
+	mt19937 gen(rd());
+	uniform_int_distribution<int> dis(min, max);
+	return dis(gen);
+}   
+
+double calcX(double coor, double width, bool left) {
+    return left ? coor - width / 2 : coor + width / 2;
+}
+
+
+vector<Ward> genWard(){
+	vector<Ward> wards;
+	ifstream file("data/hospital.txt");
+	string line;
+	if(file.is_open()){
+			getline(file,line);
+			for (int i=2;i<11;i++){
+				vector<Point> edges;
+				Point gate1,gate2,point1,point2,point3,point4;
+				double width;
+				string ward_name;
+				if(!getline(file,line)){
+					break;
+				}
+				istringstream iss(line);
+				iss>>gate1.x >>gate1.y;
+				iss>>gate2.x >> gate2.y;
+				iss>> width;
+				iss>> ward_name;
+				point1.x=calcX( gate1.x ,width,true);
+				point1.y=gate1.y;
+				edges.push_back(point1);
+
+
+				point2.x=calcX(gate2.x,width, true);
+				point2.y=gate1.y;
+				edges.push_back(point2);
+
+				point3.x=calcX( gate2.x ,width,true);
+				point3.y=gate2.y;
+				edges.push_back(point1);
+				
+				point4.x=calcX( gate2.x ,width,true);
+				point4.y=gate2.y;
+				edges.push_back(point4);
+				wards.push_back(Ward(ward_name,gate1,gate2,edges));
+
+				
+
+
+				
+
+			};
+			file.close();
+
+
+	}
+	else cout<< "File not found";
+	return wards;
+
+
+
+
+}
+
+vector<Event> genEvent(){
+	vector<Event> allEvents;
+	vector<double> allTimeDistance;
+	ifstream f1("data/timeBetweenEventDistribution");
+	string line;
+	while(getline(f1,line)){
+		allTimeDistance.push_back(stod(line));
+	}
+
+	
+	f1.close();
+	
+
+
+	ifstream file("data/event_distribution");
+	string line;
+	if(file.is_open()){
+		int i=0;
+		while(getline(file,line)){
+			Event event;
+			istringstream iss(line);
+			vector<double> instensity;
+			double value;
+			while(iss>>value)	instensity.push_back(value);
+			event.setIntensity(instensity);
+			event.setTime(allTimeDistance[i]);
+			i++;
+			
+			allEvents.push_back(event);
+
+		}
+		file.close();
+	}
+	else cout<<"File not found";
+	return allEvents;
+
+}
+
+
+
+vector<Pedestrians> genPedestrians(){
+	ifstream file("data/input.json");
+	json inputData= json::parse(file);
+	int ID=0;
+	  double lechChuan = randomDouble(1 - (double)inputData["experimentalDeviation"]["value"] / 100,
+	   1 + (double)inputData["experimentalDeviation"]["value"] / 100);
+	vector<double> ages;
+	ifstream f1("data/age_distribution");
+	string line;
+	while(getline(f1,line)){
+		ages.push_back(stod(line));
+
+	};
+	f1.close();
+
+	vector<Pedestrians> pedestrians;
+	vector<Event> events= genEvent();
+
+	// chia tỉ lệ :500=160+190+150
+
+	// chia nhỏ:75+95+100+90+113+37 
+	Pesonality open, neurotic;
+    open.setLambda(double(inputData["personalityDistribution"]["distribution"]["open"]["lambda"]));
+    open.setPositiveEmotionThreshold(double(inputData["personalityDistribution"]["distribution"]["open"]["positiveEmotionThreshold"]));
+    open.setNegativeEmotionThreshold(double(inputData["personalityDistribution"]["distribution"]["open"]["negativeEmotionThreshold"]));
+    neurotic.setLambda(double(inputData["personalityDistribution"]["distribution"]["neurotic"]["lambda"]));
+    neurotic.setPositiveEmotionThreshold(double(inputData["personalityDistribution"]["distribution"]["neurotic"]["positiveEmotionThreshold"]));
+    neurotic.setNegativeEmotionThreshold(double(inputData["personalityDistribution"]["distribution"]["neurotic"]["negativeEmotionThreshold"]));
+
+
+	double NoDisabilityNoOvertaking_velocity = double(inputData["walkability"]["distribution"]["noDisabilityNoOvertaking"]["velocity"]) * lechChuan;
+    double NoDisabilityOvertaking_velocity = double(inputData["walkability"]["distribution"]["noDisabilityOvertaking"]["velocity"]) * lechChuan;
+    double Crutches_velocity = double(inputData["walkability"]["distribution"]["crutches"]["velocity"]) * lechChuan;
+    double Sticks_velocity = double(inputData["walkability"]["distribution"]["sticks"]["velocity"]) * lechChuan;
+    double Wheelchair_velocity = double(inputData["walkability"]["distribution"]["wheelchair"]["velocity"]) * lechChuan;
+    double Blind_velocity = double(inputData["walkability"]["distribution"]["blind"]["velocity"]) * lechChuan;
+	int tiLePesdetrian[]={160,190,150};
+
+	
+				
+				while(ID++<=160){
+			  	Personnel personnel;
+                vector<Event> events;
+                personnel.setID(ID);
+				int age;
+                while (age <= 23 || age >= 61)  age = ages[randomInt(0, ages.size() - 1)];
+                personnel.setAge(age);
+				personnel.setPersonality(open);
+			 	personnel.setVelocity(ID <= 75 ? NoDisabilityNoOvertaking_velocity 
+			 									: NoDisabilityOvertaking_velocity);
+				pedestrians.push_back(personnel);
+				}
+				while(ID++<350){
+					Visitor visitor;
+                    vector<Event> events;
+                    visitor.setID(ID);
+
+                    visitor.setAge(ages[randomInt(0, ages.size() - 1)]);
+                    visitor.setPersonality(ID <= 250 ? open : (visitor.getAge() < 11 ? open : neurotic));
+                    visitor.setVelocity(ID <= 280 ? (ID <= 180 ? NoDisabilityOvertaking_velocity : Crutches_velocity) : Sticks_velocity);
+					for( int i=0; i<20;i++){
+						 Event event = events[randomInt(0, events.size() - 1)];
+						 events.push_back(event);
+					}
+					visitor.setEvents(events);
+                   pedestrians.push_back(visitor);
+				}
+				while(ID++<500){
+					
+				    Patient patient; 
+				    vector<Event> events;
+				    patient.setID(ID); 
+
+				    patient.setAge(ages[randomInt(0, ages.size() - 1)]); 
+                    patient.setPersonality(patient.getAge() < 11 ? open : neurotic);  
+                    patient.setVelocity(ID <= 113 ? Wheelchair_velocity : Blind_velocity); 
+                    
+
+                    for (int k = 0; k < 20; k++) {
+					    Event event = events[randomInt(0, events.size() - 1)];
+					    events.push_back(event);
+				    }
+				    patient.setEvents(events); 
+				    pedestrians.push_back(patient);
+
+				}
+				 ofstream outf("data/pedestrian.txt", ios::app);
+    if (!outf.is_open()) {
+        cout << "File creation failed" << endl;
+        return;
+    }
+    for (int i = 0; i < pedestrians.size(); i++) {
+        json j;
+        j["ID"] = pedestrians[i].getID();
+        j["age"] = pedestrians[i].getAge();
+        j["velocity"] = pedestrians[i].getVelocity();
+        j["personality"]["name"] = pedestrians[i].getPersonality().getLambda() == 1 ? "open" : "neurotic";
+        j["personality"]["lambda"] = pedestrians[i].getPersonality().getLambda(); 
+        j["personality"]["positiveEmotionThreshold"] = pedestrians[i].getPersonality().getPositiveEmotionThreshold();
+        j["personality"]["negativeEmotionThreshold"] = pedestrians[i].getPersonality().getNegativeEmotionThreshold(); 
+
+        j["emotion"]["pleasure"] = pedestrians[i].getEmotion().getPleasure();
+        j["emotion"]["surprise"] = pedestrians[i].getEmotion().getSurprise();
+        j["emotion"]["anger"] = pedestrians[i].getEmotion().getAnger(); 
+        j["emotion"]["fear"] = pedestrians[i].getEmotion().getFear(); 
+        j["emotion"]["hate"] = pedestrians[i].getEmotion().getHate(); 
+        j["emotion"]["sad"] = pedestrians[i].getEmotion().getSad();
+		
+        j["events"] = pedestrians[i].getEvents();
+
+        outf << j << endl;
+	}
+    outf.close();
+
+    cout << "Pedestrian generation successful" << endl;
+    return;
+}
+					
+
+
+		
+
+	
+	
+
+
+
+
+
+
+
+
+	
+
+
+
---
+++ src/pedastrian/pedestrian.h
@@ -0,0 +1,166 @@
+
+#define PEDESTRIAN_H
+
+
+
+#include <vector>
+#include <string>
+using namespace std;
+
+struct Point{
+    double x,y;
+};
+
+class Ward{
+    private: 
+        string nameOfWard;
+        Point enter ;
+        Point exit;
+        vector<Point> wallBound;
+    public:
+    Ward(){};
+    Ward(string nameOfward, Point enter, Point exit, vector<Point> wallBound){};
+
+    void setNameOfWard(){ this->nameOfWard= nameOfWard;}
+    void setEnter(){ this->enter=enter;}
+    void setExit(){ this->exit=exit;}
+    void setWallBound(){this->wallBound=wallBound;}
+
+    string getNameOfWard() {return this->nameOfWard ;}
+    Point getEnter(){ return this->enter;}
+    Point getExit(){ return this->exit;}
+    vector<Point> getWallBound(){ return this->wallBound;}
+    
+};
+
+class Emotion{
+    private:
+        double pleasure;
+        double surprise;
+        double anger;
+        double fear;
+        double hate;
+        double sad;
+        public:
+    Emotion() : pleasure(0.75), surprise(0.5), anger(-0.2), fear(-0.2), hate(-0.4), sad(-0.4) {}
+
+    double getPleasure() const { return pleasure; }
+    double getSurprise() const { return surprise; }
+    double getAnger() const { return anger; }
+    double getFear() const { return fear; }
+    double getHate() const { return hate; }
+    double getSad() const { return sad; }
+};
+
+class Pesonality{
+    private:
+    double lambda;
+    double positiveEmotionThreshold;
+    double negativeEmotionThreshold;
+
+public:
+    double getLambda() const { return lambda; }
+    double getPositiveEmotionThreshold() const { return positiveEmotionThreshold; }
+    double getNegativeEmotionThreshold() const { return negativeEmotionThreshold; }
+
+    void setLambda(double lambda) { this -> lambda = lambda; }
+    void setPositiveEmotionThreshold(double positiveEmotionThreshold) { this -> positiveEmotionThreshold = positiveEmotionThreshold; }
+    void setNegativeEmotionThreshold(double negativeEmotionThreshold) { this -> negativeEmotionThreshold = negativeEmotionThreshold; }
+};
+
+class Event {
+private:
+    double time;
+    vector<double> intensity;
+public:
+    double getTime() const { return time; }
+    vector<double> getIntensity() const { return intensity; }
+
+    void setTime(double time) { this -> time = time; }
+    void setIntensity(vector<double> intensity) { this -> intensity = intensity; }
+};
+
+enum Walkability{noDisability, crutches, sticks, wheelchairs , blind};
+
+class AGVEvents : Event{};
+class Pedestrians{
+    private:
+    int ID;
+    Ward start,end;
+    vector<Ward> journey;
+    double velocity;
+    Pesonality personality;
+    Emotion emotion;
+    vector<Event> events;
+    double walkingTime, distance,age;
+    AGVEvents impactOfAGV ;
+    Point tempPoint; 
+
+    public:
+    Pedestrians(){};
+    Pedestrians(int ID, Ward start, Ward end, double velocity, Pesonality personality,
+     Emotion emotion , vector<Event> &events, double walkingTime,
+     double distance, double age, AGVEvents impactOfAGV , Point tempPoint){};
+    
+ int getID() const { return ID; }
+  void setID(int id) { ID = id; }
+
+  const Ward& getStart() const { return this->start; }
+  void setStart(const Ward& newStart) { this->start = newStart; }
+
+  const Ward& getEnd() const { return this->end; }
+  void setEnd(const Ward& newEnd) { this->end = newEnd; }
+
+  const vector<Ward>& getJourney() const { return this->journey; }
+  void setJourney(const vector<Ward>& newJourney) {this-> journey = newJourney; }
+
+  double getVelocity() const { return this-> velocity; }
+  void setVelocity(double newVelocity) {this-> velocity = newVelocity; }
+ 
+  const Pesonality& getPersonality() const { return this-> personality; }
+  void setPersonality(const Pesonality& newPersonality) { this->personality = newPersonality; }
+
+  const Emotion& getEmotion() const { return this->emotion; }
+  void setEmotion(const Emotion& newEmotion) {this-> emotion = newEmotion; }
+
+  vector<vector<double>> getEvents() { 
+        vector<vector<double>> eventPerEmotion(6,vector<double>(20));
+        for (int i = 0; i < 6; i++) {
+            for (int j = 0; j < 20; j++) {
+                eventPerEmotion[i][j] = events[j].getIntensity()[i];
+            }
+        }
+        return eventPerEmotion;
+    }
+
+  void setEvents(const vector<Event>& newEvents) {this-> events = newEvents; }
+
+  double getWalkingTime() const { return this->walkingTime; }
+  void setWalkingTime(double newWalkingTime) { this->walkingTime = newWalkingTime; }
+
+  double getDistance() const { return this->distance; }
+  void setDistance(double newDistance) { this->distance = newDistance; }
+
+  double getAge() const { return this->age; }
+  void setAge(double newAge) { this->age = newAge; }
+
+  
+  const AGVEvents& getImpactofAGV() const { return this->impactOfAGV; }
+  void setImpactofAGV(const AGVEvents& newImpactofAGV) {this-> impactOfAGV = newImpactofAGV; }
+
+  
+  const Point& getTempPoint() const { return this->tempPoint; }
+  void setTempPoint(const Point& newTempPoint) { this->tempPoint = newTempPoint; }
+};
+
+class Patient : public Pedestrians {
+    Walkability walkability;
+};
+
+class Visitor : public Pedestrians {
+    Walkability walkability;
+};
+
+class Personnel : public Pedestrians {
+    Walkability walkability;
+};
